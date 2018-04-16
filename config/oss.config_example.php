657d9dc1cc3e6b391eebfe528645948a9aa23219
YXuuan <YXuuan@outlook.com>
2018-04-16 22:26:12 +0800

    优化config对于git的逻辑

---
+++ config/oss.config_example.php
@@ -0,0 +1,12 @@
+<?php
+
+final class OSSConfig
+{
+    const OSS_ACCESS_ID = '';
+    const OSS_ACCESS_KEY = '';
+    const OSS_ENDPOINT = '';
+    const OSS_ENDPOINT_IS_CNAME = false;
+    const OSS_BUCKET = '';
+    const OSS_ROOT_DIR = "";
+    const OSS_SIGNEDURL_TIMEOUT = 3600;
+}
