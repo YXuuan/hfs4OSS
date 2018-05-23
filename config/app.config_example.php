a180df1900ce51d77a2a03a8ee82d64112083332
YXuuan <YXuuan@outlook.com>
2018-05-23 21:01:16 +0800

    优化配置文件结构

--- config/app.config_example.php
+++ config/app.config_example.php
@@ -1,13 +1,30 @@
 <?php
 
-final class APPConfig
-{
-    const OSS_ACCESS_ID = 'LTAIfsJiANtgIUug';
-    const OSS_ACCESS_KEY = 'Tu7vB4DgSqRQC71iCO5nBUIvdPs5u5';
-    const OSS_ENDPOINT = 'http://oss-cn-shanghai.aliyuncs.com';     //Protocol Name required
-    const OSS_ENDPOINT_IS_CNAME = false;
-    const OSS_BUCKET = 'my-bucket';
-    const ROOT_DIR = "file-public/";
-    const SIGNEDURL_TIMEOUT = 3600;
-    const INDEX_PASSWORD = "";
-}
+return array(
+	'ROOT_DIR' => "mydir/",
+	'SIGNEDURL_TIMEOUT' => 3600,
+	'INDEX_AUTH' => array(
+		'FIRSTMET' => '请输入密码：',
+		'PASSWORD' => '2333',
+		'IFWRONG' => '我不会告诉你密码是2333的，请重新输入：'
+	),
+	'PREFIX_AUTH' => array(
+		'mydir/myphotos/' => array(
+			'FIRSTMET' => '请输入相册密码：',
+			'PASSWORD' => '2334',
+			'IFWRONG' => '密码错误，重新输入：'
+		),
+		'mydir/myvideos/' => array(
+			'FIRSTMET' => '请输入相册密码：',
+			'PASSWORD' => '2335',
+			'IFWRONG' => '你以为我会给你看吗？'
+		)
+	),
+	'FILE_AUTH' => array(
+		'mydir/something/SNIS-048.avi' => array(
+			'FIRSTMET' => '你真的想看，输密码吧？',
+			'PASSWORD' => '啧',
+			'IFWRONG' => '密码错误，重新输入：'
+		),
+	),
+);
---
+++ config/oss.config_example.php
@@ -0,0 +1,10 @@
+<?php
+
+final class APPConfig
+{
+    const ACCESS_ID = 'LTAIfsJiANtgIUug';
+    const ACCESS_KEY = 'Tu7vB4DgSqRQC71iCO5nBUIvdPs5u5';
+    const ENDPOINT = 'http://oss-cn-shanghai.aliyuncs.com';
+    const ENDPOINT_IS_CNAME = false;
+    const BUCKET = 'my-bucket';
+}
