93b149d5a050606b377b8bd4e674b9f2e0403a46
YXuuan <YXuuan@outlook.com>
2018-06-05 21:08:57 +0800

    修改重设check_var()判定逻辑，新增str_replace_limit()

--- app/function/app.function.php
+++ app/function/app.function.php
@@ -1,6 +1,19 @@
 <?php
 
 function check_var($var, $default = ''){
-	return (isset($var) && $var !== "");
+    return (isset($var) && $var !== "");
     //return((isset($var) and !empty($var)) ? $var : (!empty($default) ? $default : null));
+}
+
+function str_replace_limit($search, $replace, $subject, $limit = -1){
+    // constructing mask(s)...
+    if(is_array($search)){
+        foreach($search as $k => $v) {
+            $search[$k] = '`'.preg_quote($search[$k], '`').'`';
+        }
+    }else{
+        $search = '`'.preg_quote($search, '`').'`';
+    }
+    // replacement
+    return preg_replace($search, $replace, $subject, $limit);
 }
